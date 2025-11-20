458f75eed0e280585c0180c8b6c1c1e3e1550d40
ubterzioglu <ubterzioglu@gmail.com>
2025-11-20 20:21:10 +0100

    Selenium Basic Files Upload

---
+++ src/main/java/seleniumbasic/SelFinTest_Scroll.java
@@ -0,0 +1,42 @@
+package seleniumbasic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class SelFinTest_Scroll {
+
+
+
+    @Test
+
+    void Setup(){
+
+      //  ChromeOptions options = new ChromeOptions();
+     //   options.addArguments("--remote-allow-origins=*");
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/scroll");
+
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(name);
+        name.sendKeys("UBT");
+        WebElement date = driver.findElement(By.id("date"));
+        date.sendKeys("01/01/2022");
+
+        driver.quit();
+
+
+
+
+
+
+
+
+    }
+}
